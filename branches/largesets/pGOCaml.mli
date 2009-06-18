eed6b4cd4acd1c67be9895b5577fbb2858c73abc
darioteixeira <darioteixeira@1cd37ec5-ba40-0410-8527-ddbf52fb9f18>
2009-06-18 12:22:22 +0000

    Removed largesets branches (it is already incorporated into trunk)

--- branches/largesets/pGOCaml.mli
+++
@@ -1,6 +0,0 @@
-(* PG'OCaml is a set of OCaml bindings for the PostgreSQL database.
- * $Id: pGOCaml.mli,v 1.14 2007-10-14 14:52:27 rich Exp $
- *)
-
-include PGOCaml_generic.PGOCAML_GENERIC
-  with type 'a monad = 'a
